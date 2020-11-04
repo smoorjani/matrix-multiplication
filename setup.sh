1e5afd9d8562ce7ea44efd7fbe66d3708eca42f9
Samraj Moorjani <samrajmoorjani@gmail.com>
2020-11-04 09:04:29 -0500

    Added python.h library fixes in make and setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 wget https://download.pytorch.org/libtorch/cu102/libtorch-shared-with-deps-1.7.0.zip
 unzip libtorch-shared-with-deps-1.7.0.zip
 
+sudo apt-get install python3-dev
+
+cd cublas_mm
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH=$PWD/../../libtorch ..
