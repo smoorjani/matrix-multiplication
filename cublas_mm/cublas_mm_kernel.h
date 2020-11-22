239cfc54dbb704ae88c50e4a3bf2299ef6b0ad48
Samraj Moorjani <samrajmoorjani@gmail.com>
2020-11-22 14:02:04 -0500

    Overhauled mm.

--- cublas_mm/cublas_mm_kernel.h
+++ cublas_mm/cublas_mm_kernel.h
@@ -56,63 +56,68 @@ void mmul_wrapper(const float *A, const float *B, float *C,
 				  const int A_rows, const int A_cols,
 				  const int B_rows, const int B_cols,
 				  const int C_rows, const int C_cols) {
-	
-	// Allocate 3 arrays on GPU
+
+	// initialize data
+
+	for (size_t i = 0; i < A_rows; i++){
+		for (size_t j = 0; j < A_cols; j++){
+			std::cout << A[ci(i,j,A_cols)] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	for (size_t i = 0; i < B_rows; i++){
+		for (size_t j = 0; j < B_cols; j++){
+			std::cout << B[ci(i,j,B_cols)] << " ";
+		}
+		std::cout << "\n";
+	}
+
 	float *gpu_A, *gpu_B, *gpu_C;
 	cudaMalloc(&gpu_A, A_rows * A_cols * sizeof(float));
 	cudaMalloc(&gpu_B, B_rows * B_cols * sizeof(float));
 	cudaMalloc(&gpu_C, C_rows * C_cols * sizeof(float));
 
-	float *h_A = (float *)malloc(A_rows * A_cols * sizeof(float));
-	float *h_B = (float *)malloc(B_rows * B_cols * sizeof(float));
-	float *h_C = (float *)malloc(C_rows * C_cols * sizeof(float));
+	cudaMemcpy(gpu_A, A, A_rows * A_cols * sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_B, B, B_rows * B_cols * sizeof(float),cudaMemcpyHostToDevice);
 
-	// Fill the arrays A and B on GPU with random numbers
-	// fill_rand(gpu_A, A_rows, A_cols);
-	// fill_rand(gpu_B, B_rows, B_cols);
-	// cudaMemcpy(gpu_A, A, A_rows * A_cols * sizeof(float),cudaMemcpyHostToDevice);
-	// cudaMemcpy(gpu_B, B, B_rows * B_cols * sizeof(float),cudaMemcpyHostToDevice);
-
-	for (int i = 0; i < A_rows; i++) {
-		for (int j = 0; j < A_cols; j++) {
-			h_A[ci(i,j,A_cols)] = i+j;
-		}
-	}
-
-	for (int i = 0; i < B_rows; i++) {
-		for (int j = 0; j < B_cols; j++) {
-			h_A[ci(i,j,B_cols)] = i+j;
-		}
-	}
-
-	cudaMemcpy(h_A, gpu_A, A_rows * A_cols * sizeof(float),cudaMemcpyDeviceToHost);
-	cudaMemcpy(h_B, gpu_B, B_rows * B_cols * sizeof(float),cudaMemcpyDeviceToHost);
-	print_matrix(h_A, A_rows, A_cols);
-	print_matrix(h_B, B_rows, B_cols);
-
-	const float a = 1;
-	const float b = 0;
-	const float *alpha = &a;
-	const float *beta = &b;
+	// for (size_t i = 0; i < C_rows; i++)
+	// 	for (size_t j = 0; j < C_cols; j++)
+	// 		C[ci(i,j,C_cols)] = 0;
 
 	cublasHandle_t handle;
-	cublasCreate(&handle);
-	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, B_cols, A_rows, A_cols, alpha, gpu_A, B_cols, gpu_B, A_cols, beta, gpu_C, B_cols);
-	cublasDestroy(handle);
+
+		/* Initialize CUBLAS */
+	cublasStatus_t status = cublasCreate(&handle);
+	if (status != CUBLAS_STATUS_SUCCESS) {
+		std::cerr << "!!!! CUBLAS initialization error\n";
+	}
+	
+	float alpha = 1.0f;
+	float beta = 0.0f;
+	status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
+										B_cols, A_rows, A_cols, 
+										&alpha, gpu_B, B_cols, 
+												gpu_A, A_cols, 
+										&beta,  gpu_C, B_cols);// B_cols  x A_rows
+	if (status != CUBLAS_STATUS_SUCCESS) {
+		std::cerr << "!!!! kernel execution error.\n";
+	}
 
 	cudaMemcpy(C, gpu_C, C_rows * C_cols * sizeof(float), cudaMemcpyDeviceToHost);
 
-	cudaMemcpy(h_C, gpu_C, C_rows * C_cols * sizeof(float),cudaMemcpyDeviceToHost);
-	print_matrix(h_C, C_rows, C_cols);
+	for (size_t i = 0; i < C_rows; i++){
+		for (size_t j = 0; j < C_cols; j++){
+			std::cout << C[ci(i,j,C_cols)] << " ";
+		}
+		std::cout << "\n";
+	}
 
-	//Free GPU memory
-	cudaFree(gpu_A);
-	cudaFree(gpu_B);
-	cudaFree(gpu_C);
-
-	free(h_A);
-	free(h_B);
-	free(h_C);
+	status = cublasDestroy(handle);
+	if (status != CUBLAS_STATUS_SUCCESS) {
+		std::cerr << "!!!! shutdown error (A)\n";
+	}
+		
 }
 
 void random_mmul() {
@@ -157,3 +162,63 @@ void random_mmul() {
 }
 
 #endif // __CUBLAS_MM_KERNEL_H__
+
+
+/*
+// Allocate 3 arrays on GPU
+	float *gpu_A, *gpu_B, *gpu_C;
+	cudaMalloc(&gpu_A, A_rows * A_cols * sizeof(float));
+	cudaMalloc(&gpu_B, B_rows * B_cols * sizeof(float));
+	cudaMalloc(&gpu_C, C_rows * C_cols * sizeof(float));
+
+	float *h_A = (float *)malloc(A_rows * A_cols * sizeof(float));
+	float *h_B = (float *)malloc(B_rows * B_cols * sizeof(float));
+	float *h_C = (float *)malloc(C_rows * C_cols * sizeof(float));
+
+	// Fill the arrays A and B on GPU with random numbers
+	// fill_rand(gpu_A, A_rows, A_cols);
+	// fill_rand(gpu_B, B_rows, B_cols);
+	// cudaMemcpy(gpu_A, A, A_rows * A_cols * sizeof(float),cudaMemcpyHostToDevice);
+	// cudaMemcpy(gpu_B, B, B_rows * B_cols * sizeof(float),cudaMemcpyHostToDevice);
+
+	for (int i = 0; i < A_rows; i++) {
+		for (int j = 0; j < A_cols; j++) {
+			h_A[ci(i,j,A_cols)] = i+j;
+		}
+	}
+
+	for (int i = 0; i < B_rows; i++) {
+		for (int j = 0; j < B_cols; j++) {
+			h_B[ci(i,j,B_cols)] = i+j;
+		}
+	}
+
+	cudaMemcpy(h_A, gpu_A, A_rows * A_cols * sizeof(float),cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_B, gpu_B, B_rows * B_cols * sizeof(float),cudaMemcpyDeviceToHost);
+	print_matrix(h_A, A_rows, A_cols);
+	print_matrix(h_B, B_rows, B_cols);
+
+	const float a = 1;
+	const float b = 0;
+	const float *alpha = &a;
+	const float *beta = &b;
+
+	cublasHandle_t handle;
+	cublasCreate(&handle);
+	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, B_cols, A_rows, A_cols, alpha, gpu_A, B_cols, gpu_B, A_cols, beta, gpu_C, B_cols);
+	cublasDestroy(handle);
+
+	cudaMemcpy(C, gpu_C, C_rows * C_cols * sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaMemcpy(h_C, gpu_C, C_rows * C_cols * sizeof(float),cudaMemcpyDeviceToHost);
+	print_matrix(h_C, C_rows, C_cols);
+
+	//Free GPU memory
+	cudaFree(gpu_A);
+	cudaFree(gpu_B);
+	cudaFree(gpu_C);
+
+	free(h_A);
+	free(h_B);
+	free(h_C);
+*/
