6ba66691cc7d025322463b3ab9b4a605ebe994c3
Samraj Moorjani <samrajmoorjani@gmail.com>
2020-11-23 11:14:21 -0500

    Created torch inplace function for autograd.

--- cublas_mm/cublas_mm_kernel.h
+++ cublas_mm/cublas_mm_kernel.h
@@ -15,13 +15,6 @@ https://gist.github.com/peterwittek/6303527
 #include <cublas_v2.h>
 #include <curand.h>
 
-#define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1))
-#define IDX2C(i,j,ld) (((j)*(ld))+(i))
-
-int ci(int row, int column, int nColumns) {
-  return row*nColumns+column;
-}
-
 // Print matrix A
 void print_matrix(const float *A, int A_rows, int A_cols) {
     for(int i = 0; i < A_rows; ++i){
@@ -44,6 +37,10 @@ void mmul_wrapper(const float *A, const float *B, float *C,
 				  const int B_rows, const int B_cols,
 				  const int C_rows, const int C_cols) {
 
+	if (A_cols != B_rows || C_rows != A_rows || C_cols != B_cols) {
+		std::cerr << "Invalid dimensions.";
+	}
+
 	float *gpu_A, *gpu_B, *gpu_C;
 	cudaMalloc(&gpu_A, A_rows * A_cols * sizeof(float));
 	cudaMalloc(&gpu_B, B_rows * B_cols * sizeof(float));
@@ -53,14 +50,13 @@ void mmul_wrapper(const float *A, const float *B, float *C,
 	cudaMemcpy(gpu_B, B, B_rows * B_cols * sizeof(float),cudaMemcpyHostToDevice);
 
 	cublasHandle_t handle;
-
 	cublasStatus_t status = cublasCreate(&handle);
 	if (status != CUBLAS_STATUS_SUCCESS) {
-		std::cerr << "!!!! CUBLAS initialization error\n";
+		std::cerr << "cuBLAS initialization error.";
 	}
-	
-	float alpha = 1.0f;
-	float beta = 0.0f;
+
+	float alpha = 1.0;
+	float beta = 0.0;
 	status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
 										B_cols, A_rows, A_cols, 
 										&alpha, gpu_B, B_cols, 
--- test_op/fc_layer.py
+++ test_op/fc_layer.py
@@ -3,6 +3,33 @@ import torch
 import torch.nn as nn
 import cublas_mm
 
+from torch.autograd.function import InplaceFunction
+
+'''
+https://cs231n.github.io/optimization-2/#mat
+https://gist.github.com/anonymous/49c10bc17ac4a97307d52c07d01a2870
+'''
+
+class cublasMM(InplaceFunction):
+
+    @staticmethod
+    def forward(ctx, m1, m2):
+        ctx.save_for_backward(m1, m2)
+        return cublas_mm.mmul(m1, m2)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        m1, m2 = ctx.saved_variables
+        grad_m1 = grad_m2 = None
+
+        if ctx.needs_input_grad[0]:
+            grad_m1 = cublas_mm.mmul(grad_output, m2.t())
+        
+        if ctx.needs_input_grad[1]:
+            grad_m2 = cublas_mm.mmul(m1.t(), grad_output)
+        
+        return grad_m1, grad_m2
+
 class regLinear(nn.Module):
     def __init__(self, in_features, out_features, bias=True):
         super(regLinear, self).__init__()
@@ -73,7 +100,7 @@ class cublasLinear(nn.Module):
         if y != self.in_features:
             print('Invalid dimensions')
             return 0
-        output = cublas_mm.mmul(inp, self.weight.t())
+        output = cublasMM.apply(self.weight.t().t(), inp.t())
 
         if self.bias is not None:
             output += self.bias
