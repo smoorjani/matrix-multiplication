811146ad066f6bb50b842a2649a4c7fff7e88d32
Samraj Moorjani <samrajmoorjani@gmail.com>
2020-11-22 23:07:21 -0500

    Updated setup for torchvision build.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ cd pytorch/
 pip3 install cython
 pip3 install -r requirements.txt
 
+# build libtorch and pytorch
 mkdir build_libtorch && cd build_libtorch
 export TORCH_CUDA_ARCH_LIST="7.0"
 python3 ../tools/build_libtorch.py
@@ -17,3 +18,12 @@ cd /home/user/src/pytorch
 python3 setup.py install
 
 cd /home/user/src
+
+# build torchvision for MNIST data
+git clone --recursive https://github.com/pytorch/vision
+cd vision/
+python3 setup.py install
+
+# alias for python3
+echo "alias python=python3" >> ~/.bashrc
+source ~/.bashrc
