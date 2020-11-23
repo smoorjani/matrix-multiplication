5fb6fd2cd056360efe15a1b8eccf66639e7e48f0
Samraj Moorjani <samrajmoorjani@gmail.com>
2020-11-23 12:28:29 -0500

    Modified tests and split models.

---
+++ test_op/regular_fc_layer.py
@@ -0,0 +1,44 @@
+import math
+import torch
+import torch.nn as nn
+
+class regLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(regLinear, self).__init__()
+        torch.manual_seed(0)
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.bias = bias
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+        
+    def forward(self, inp):
+        x, y = inp.shape
+        if y != self.in_features:
+            print('Invalid dimensions')
+            return 0
+        output = inp @ self.weight.t()
+
+        if self.bias is not None:
+            output += self.bias
+        ret = output
+        return ret
+    
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
--- test_op/test_layer.py
+++ test_op/test_layer.py
@@ -15,7 +15,8 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torchvision import datasets, transforms
 
-from fc_layer import regLinear, cublasLinear
+from cublas_fc_layer import cublasLinear
+from regular_fc_layer import regLinear
 
 # Seed to reproduce results
 np.random.seed(0)
@@ -26,75 +27,135 @@ batch_size=1
 learning_rate=0.01
 epochs=1
 log_interval=10
+random_test = True
 
-# Regular MM operation 
-class regNet(nn.Module):
-    def __init__(self):
-        super(regNet, self).__init__()
-        self.fc1 = regLinear(28*28,10)
+if random_test:
+    layer_size=1024
 
-    def forward(self, x):
-        x = self.fc1(x)
-        return F.log_softmax(x)
+    # Regular MM operation 
+    class regNet(nn.Module):
+        def __init__(self):
+            super(regNet, self).__init__()
+            self.fc1 = regLinear(layer_size*layer_size,10)
 
-# Cublas MM operation
-class cubNet(nn.Module):
-    def __init__(self):
-        super(cubNet, self).__init__()
-        self.fc1 = cublasLinear(28*28,10)
+        def forward(self, x):
+            x = self.fc1(x)
+            return F.log_softmax(x)
 
-    def forward(self, x):
-        x = self.fc1(x)
-        return F.log_softmax(x)
+    # Cublas MM operation
+    class cubNet(nn.Module):
+        def __init__(self):
+            super(cubNet, self).__init__()
+            self.fc1 = cublasLinear(layer_size*layer_size,10)
 
-reg_net = regNet()
-cub_net = cubNet()
+        def forward(self, x):
+            x = self.fc1(x)
+            return F.log_softmax(x)
 
-for net in [reg_net, cub_net]:
-    # Load in MNIST data
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ])),
-            batch_size=batch_size, shuffle=False)
+    reg_net = regNet()
+    cub_net = cubNet()
 
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, 
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ])),
-            batch_size=batch_size, shuffle=False)
+    data = torch.rand(layer_size,layer_size).view(-1,layer_size*layer_size)
 
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
-    criterion = nn.NLLLoss()
 
-    # main training loop
-    for epoch in range(epochs):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            if batch_idx == 10:
-                # break because of extended execution time
-                break
-            
-            data, target = Variable(data), Variable(target)
-            data = data.view(-1, 28*28)
+    for net in [reg_net, cub_net]:
+        # Load in MNIST data
 
-            optimizer.zero_grad()
-            net_out = net(data)
+        print('\n====Initial Parameters=====\n')
+        for param in net.parameters():
+            print(param)
 
-            loss = criterion(net_out, target)
-            loss.backward()
-            optimizer.step()
+        optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+        criterion = nn.NLLLoss()
+                
+        target = torch.tensor([1])
 
-            if batch_idx % log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                        100. * batch_idx / len(train_loader), loss))
+        optimizer.zero_grad()
+        net_out = net(data)
 
-    for param in net.parameters():
-        print(param)
+        loss = criterion(net_out, target)
+        loss.backward()
+        optimizer.step()
+
+        print('\n====Trained Parameters=====\n')
+        for param in net.parameters():
+            print(param)
+
+else:
+
+    # Regular MM operation 
+    class regNet(nn.Module):
+        def __init__(self):
+            super(regNet, self).__init__()
+            self.fc1 = regLinear(28*28,10)
+
+        def forward(self, x):
+            x = self.fc1(x)
+            return F.log_softmax(x)
+
+    # Cublas MM operation
+    class cubNet(nn.Module):
+        def __init__(self):
+            super(cubNet, self).__init__()
+            self.fc1 = cublasLinear(28*28,10)
+
+        def forward(self, x):
+            x = self.fc1(x)
+            return F.log_softmax(x)
+
+    reg_net = regNet()
+    cub_net = cubNet()
+
+    for net in [reg_net, cub_net]:
+        print('\n====Initial Parameters=====\n')
+        for param in net.parameters():
+            print(param)
+
+        # Load in MNIST data
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('../data', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.1307,), (0.3081,))
+                ])),
+                batch_size=batch_size, shuffle=False)
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('../data', train=False, 
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.1307,), (0.3081,))
+                ])),
+                batch_size=batch_size, shuffle=False)
+
+        optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+        criterion = nn.NLLLoss()
+
+        # main training loop
+        for epoch in range(epochs):
+            for batch_idx, (data, target) in enumerate(train_loader):
+                if batch_idx == 10:
+                    # break because of extended execution time
+                    break
+                
+                data, target = Variable(data), Variable(target)
+                data = data.view(-1, 28*28)
+
+                optimizer.zero_grad()
+                net_out = net(data)
+
+                loss = criterion(net_out, target)
+                loss.backward()
+                optimizer.step()
+
+                # if batch_idx % log_interval == 0:
+                #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                #         epoch, batch_idx * len(data), len(train_loader.dataset),
+                #             100. * batch_idx / len(train_loader), loss))
+
+        print('\n====Trained Parameters=====\n')
+        for param in net.parameters():
+            print(param)
 
 
     
