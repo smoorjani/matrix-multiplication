a2068335b1fa0b67df7fdb269c989efd4ff1378c
Samraj Moorjani <samrajmoorjani@gmail.com>
2021-01-02 17:25:01 -0500

    Converted double to float for compatability with cuBLAS.

--- cublas_mm/cublas_mm_kernel.cu
+++ cublas_mm/cublas_mm_kernel.cu
@@ -11,14 +11,14 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 
-void cublas_mm_wrapper(double *h_A, int h_A_rows, int h_A_cols, double *h_B, int h_B_rows, int h_B_cols) {
+void cublas_mm_wrapper(float *h_A, int h_A_rows, int h_A_cols, float *h_B, int h_B_rows, int h_B_cols) {
     const int m = h_A_rows;
     const int k = h_A_cols;
     const int n = h_B_cols;
 
     int h_C_rows = h_A_rows;
     int h_C_cols = h_B_cols;
-    double *h_C = (double *)malloc(h_C_rows * h_C_cols * sizeof(*h_C));
+    float *h_C = (float *)malloc(h_C_rows * h_C_cols * sizeof(*h_C));
 
     if (h_A_cols != h_B_rows) {
 		std::cerr << "Invalid dimensions.";
@@ -30,13 +30,13 @@ void cublas_mm_wrapper(double *h_A, int h_A_rows, int h_A_cols, double *h_B, int
 		std::cerr << "cuBLAS initialization error.";
     }
     
-    double *d_A, *d_B, *d_C;
-	cudaMalloc(&d_A, h_A_rows * h_A_cols * sizeof(double));
-	cudaMalloc(&d_B, h_B_rows * h_B_cols * sizeof(double));
-	cudaMalloc(&d_C, h_C_rows * h_C_cols * sizeof(double));
+    float *d_A, *d_B, *d_C;
+	cudaMalloc(&d_A, h_A_rows * h_A_cols * sizeof(float));
+	cudaMalloc(&d_B, h_B_rows * h_B_cols * sizeof(float));
+	cudaMalloc(&d_C, h_C_rows * h_C_cols * sizeof(float));
 
-	cudaMemcpy(d_A, h_A, h_A_rows * h_A_cols * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, h_B_rows * h_B_cols * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_A, h_A, h_A_rows * h_A_cols * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, h_B_rows * h_B_cols * sizeof(float), cudaMemcpyHostToDevice);
 
     float alpha = 1.0;
 	float beta = 0.0;
@@ -52,7 +52,7 @@ void cublas_mm_wrapper(double *h_A, int h_A_rows, int h_A_cols, double *h_B, int
 		std::cerr << "Kernel execution error.";
 	}
 
-	cudaMemcpy(h_C, d_C, C_rows * C_cols * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_C, d_C, h_C_rows * h_C_cols * sizeof(float), cudaMemcpyDeviceToHost);
 
 	status = cublasDestroy(handle);
 	if (status != CUBLAS_STATUS_SUCCESS) {
@@ -76,7 +76,7 @@ int main()
     // (3 x 4) x (4 x 2) = 3 x 2
     int h_A_rows = 3;
     int h_A_cols = 4;
-    double *h_A = (double *)malloc(h_A_rows * h_A_cols * sizeof(*h_A));
+    float *h_A = (float *)malloc(h_A_rows * h_A_cols * sizeof(*h_A));
 
     // Column-major ordering
     h_A[0] = 1.0f;
@@ -95,7 +95,7 @@ int main()
 
     int h_B_rows = 4;
     int h_B_cols = 2;
-    double *h_B = (double *)malloc(h_B_rows * h_B_cols * sizeof(*h_B));
+    float *h_B = (float *)malloc(h_B_rows * h_B_cols * sizeof(*h_B));
 
     // Column-major ordering
     h_B[0] = 1.0f;
