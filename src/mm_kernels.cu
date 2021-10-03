7ce035ee3d9b55dcbff8db804c82f3e1e5273119
Samraj Moorjani <samrajmoorjani@gmail.com>
2021-10-03 12:42:14 -0500

    Cleaned code and updated 4d bmm to include optimizations.

--- src/mm_kernels.cu
+++ src/mm_kernels.cu
@@ -257,39 +257,12 @@ void cublas_bmm_wrapper(cublasHandle_t handle,
 
     timestamp_t t0 = get_timestamp();
     
-    /*
-    float *a_devPtr, *b_devPtr, *c_devPtr;
-    size_t a_pitch, b_pitch, c_pitch;
-    cudaMallocPitch((void**)&a_devPtr, &a_pitch, batch_dim * sizeof(float), a_size);
-    cudaMallocPitch((void**)&b_devPtr, &b_pitch, batch_dim * sizeof(float), b_size);
-    cudaMallocPitch((void**)&c_devPtr, &c_pitch, batch_dim * sizeof(float), c_size);
-    */
+    // setting up device arrays for bmm
     float *d_A_arr, *d_B_arr, *d_C_arr;
     gpuErrchk(cudaMalloc(&d_A_arr, batch_dim * a_size));
     gpuErrchk(cudaMalloc(&d_B_arr, batch_dim * b_size));
     gpuErrchk(cudaMalloc(&d_C_arr, batch_dim * c_size));
-    /*
-    // setting up device arrays for bmm
-    float **d_A_arr, **d_B_arr, **d_C_arr;
-    float **h_A_arr = (float **) malloc(batch_dim * sizeof(float*));
-    float **h_B_arr = (float **) malloc(batch_dim * sizeof(float*));
-    float **h_C_arr = (float **) malloc(batch_dim * sizeof(float*));
-
-    gpuErrchk(cudaMalloc((void **)&d_A_arr, batch_dim * sizeof(float *)));
-    gpuErrchk(cudaMalloc((void **)&d_B_arr, batch_dim * sizeof(float *)));
-    gpuErrchk(cudaMalloc((void **)&d_C_arr, batch_dim * sizeof(float *)));
-
-    for (int i = 0; i < batch_dim; i++) {
-        gpuErrchk(cudaMalloc(&h_A_arr[i], a_size));
-        gpuErrchk(cudaMalloc(&h_B_arr[i], b_size));
-        gpuErrchk(cudaMalloc(&h_C_arr[i], c_size));
-    }
-
-    gpuErrchk(cudaMemcpy(d_A_arr, h_A_arr, batch_dim * sizeof(float*), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_B_arr, h_B_arr, batch_dim * sizeof(float*), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_C_arr, h_C_arr, batch_dim * sizeof(float*), cudaMemcpyHostToDevice));
-    */
-
+    
     timestamp_t t1 = get_timestamp();
     double secs = (t1 - t0) / 1000000.0L;
     printf("2d array declaration: %f\n", secs);
@@ -301,13 +274,9 @@ void cublas_bmm_wrapper(cublasHandle_t handle,
 
     // execute 1 time with a_rows threads
     dim3 thread_per_block(NUM_THREADS);
-    // dim3 A_blocks_per_grid((batch_dim * a_rows * b_rows + NUM_THREADS - 1)/NUM_THREADS);
-    // dim3 B_blocks_per_grid((batch_dim * b_rows * b_cols + NUM_THREADS - 1)/NUM_THREADS);
     dim3 C_blocks_per_grid((batch_dim * a_rows * b_cols + NUM_THREADS - 1)/NUM_THREADS);
     dim3 AB_blocks_per_grid((batch_dim * a_rows * b_rows + batch_dim * a_rows * b_cols + NUM_THREADS - 1)/NUM_THREADS);
     // <<<total threads/64 ,64>>>>
-    // packed_2d_accessor_kernel<<<A_blocks_per_grid, thread_per_block>>>(A_accessor, d_A_arr);
-    // packed_2d_accessor_kernel<<<B_blocks_per_grid, thread_per_block>>>(B_accessor, d_B_arr);
     packed_2d_accessor_kernel_combined<<<AB_blocks_per_grid, thread_per_block>>>(A_accessor, B_accessor, d_A_arr, d_B_arr);
     t1 = get_timestamp();
     secs = (t1 - t0) / 1000000.0L;
@@ -317,13 +286,7 @@ void cublas_bmm_wrapper(cublasHandle_t handle,
 
     t0 = get_timestamp();
     const float alpha = 1.0f, beta = 0.0f;
-    /*
-    cublasStatus_t status = cublasSgemmBatched(handle, CUBLAS_OP_N, CUBLAS_OP_N
-                                       , b_cols, a_rows, b_rows
-                                       , &alpha, d_B_arr, b_cols, d_A_arr, b_rows
-                                       , &beta, d_C_arr, b_cols
-                                       , batch_dim);
-    */
+
     cublasStatus_t status = cublasSgemmStridedBatched(handle, CUBLAS_OP_N, CUBLAS_OP_N
                                        , b_cols, a_rows, b_rows
                                        , &alpha, d_B_arr, b_cols, b_rows * b_cols
@@ -347,21 +310,6 @@ void cublas_bmm_wrapper(cublasHandle_t handle,
     printf("Setter: %f\n", secs);
 
     t0 = get_timestamp();
-    /*
-    for (int i = 0; i < batch_dim; i++) {
-        gpuErrchk(cudaFree(h_A_arr[i]));
-        gpuErrchk(cudaFree(h_B_arr[i]));
-        gpuErrchk(cudaFree(h_C_arr[i]));
-    }
-    gpuErrchk(cudaFree(d_A_arr));
-    gpuErrchk(cudaFree(d_B_arr));
-    gpuErrchk(cudaFree(d_C_arr));
-    
-    free(h_A_arr);
-    free(h_B_arr);
-    free(h_C_arr);
-    */
-     
     gpuErrchk(cudaFree(d_A_arr));
     gpuErrchk(cudaFree(d_B_arr));
     gpuErrchk(cudaFree(d_C_arr));
@@ -459,24 +407,11 @@ void cublas_4d_bmm_wrapper(cublasHandle_t handle,
     size_t b_size = sizeof(float) * b_rows * b_cols;
     size_t c_size = sizeof(float) * a_rows * b_cols;
 
-    float **d_A_arr, **d_B_arr, **d_C_arr;
-    float **h_A_arr = (float **) malloc(batch_dim1 * batch_dim2 * sizeof(float*));
-    float **h_B_arr = (float **) malloc(batch_dim1 * batch_dim2 * sizeof(float*));
-    float **h_C_arr = (float **) malloc(batch_dim1 * batch_dim2 * sizeof(float*));
-
-    gpuErrchk(cudaMalloc((void **)&d_A_arr, batch_dim1 * batch_dim2 * sizeof(float *)));
-    gpuErrchk(cudaMalloc((void **)&d_B_arr, batch_dim1 * batch_dim2 * sizeof(float *)));
-    gpuErrchk(cudaMalloc((void **)&d_C_arr, batch_dim1 * batch_dim2 * sizeof(float *)));
-
-    for (int i = 0; i < batch_dim1 * batch_dim2; i++) {
-        gpuErrchk(cudaMalloc(&h_A_arr[i], a_size));
-        gpuErrchk(cudaMalloc(&h_B_arr[i], b_size));
-        gpuErrchk(cudaMalloc(&h_C_arr[i], c_size));
-    }
-
-    gpuErrchk(cudaMemcpy(d_A_arr, h_A_arr, batch_dim1 * batch_dim2 * sizeof(float*), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_B_arr, h_B_arr, batch_dim1 * batch_dim2 * sizeof(float*), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_C_arr, h_C_arr, batch_dim1 * batch_dim2 * sizeof(float*), cudaMemcpyHostToDevice));
+    // setting up device arrays for bmm
+    float *d_A_arr, *d_B_arr, *d_C_arr;
+    gpuErrchk(cudaMalloc(&d_A_arr, batch_dim1 * batch_dim2 * a_size));
+    gpuErrchk(cudaMalloc(&d_B_arr, batch_dim1 * batch_dim2 * b_size));
+    gpuErrchk(cudaMalloc(&d_C_arr, batch_dim1 * batch_dim2 * c_size));
 
     auto A_accessor = d_A.packed_accessor32<float,4>();
     auto B_accessor = d_B.packed_accessor32<float,4>();
@@ -490,36 +425,29 @@ void cublas_4d_bmm_wrapper(cublasHandle_t handle,
 
     // <<<total threads/64 ,64>>>>
     packed_2d_accessor_kernel_4d<<<A_blocks_per_grid, thread_per_block>>>(A_accessor, d_A_arr);
-    cudaDeviceSynchronize();
     packed_2d_accessor_kernel_4d<<<B_blocks_per_grid, thread_per_block>>>(B_accessor, d_B_arr);
   
+    gpuErrchk(cudaDeviceSynchronize());
     const float alpha = 1.0f, beta = 0.0f;
        
-    cublasStatus_t status = cublasSgemmBatched(handle, CUBLAS_OP_N, CUBLAS_OP_N
+    cublasStatus_t status = cublasSgemmStridedBatched(handle, CUBLAS_OP_N, CUBLAS_OP_N
                                        , b_cols, a_rows, b_rows
-                                       , &alpha, d_B_arr, b_cols, d_A_arr, b_rows
-                                       , &beta, d_C_arr, b_cols
+                                       , &alpha, d_B_arr, b_cols, b_rows * b_cols
+                                       , d_A_arr, b_rows, a_rows * b_rows
+                                       , &beta, d_C_arr, b_cols, a_rows * b_cols
                                        , batch_dim1 * batch_dim2);
     
     if (status != CUBLAS_STATUS_SUCCESS)
     {
         std::cerr << "Kernel execution error.";
     }
+    gpuErrchk(cudaDeviceSynchronize());
 
     packed_2d_setter_kernel_4d<<<C_blocks_per_grid, thread_per_block>>>(C_accessor, d_C_arr);
     
-    for (int i = 0; i < batch_dim1 * batch_dim2; i++) {
-        gpuErrchk(cudaFree(h_A_arr[i]));
-        gpuErrchk(cudaFree(h_B_arr[i]));
-        gpuErrchk(cudaFree(h_C_arr[i]));
-    }
     gpuErrchk(cudaFree(d_A_arr));
     gpuErrchk(cudaFree(d_B_arr));
     gpuErrchk(cudaFree(d_C_arr));
-    
-    free(h_A_arr);
-    free(h_B_arr);
-    free(h_C_arr);
 }
 
 /*
